f6b8dfb1ead39c3aa9821541fc380827e3bc345b
czh <120715979@qq.com>
2017-09-09 22:05:45 +0800

    完善文档

--- src/main/java/com/czh/controller/TestController.java
+++ src/main/java/com/czh/controller/TestController.java
@@ -2,24 +2,14 @@ package com.czh.controller;
 
 import com.czh.dao.UserDao;
 import com.czh.entity.User;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
+
 
 /**
  * Created by czh on 17-6-4.
